03091dc14bde25e555900e5c6bcdd82bd67746e3
HHuser1 <156039838+HHuser1@users.noreply.github.com>
2024-01-09 11:31:10 +0800

    Add files via upload

---
+++ fig 3a.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 11:22:11 2023
+
+@author: HH
+"""
+
+import netCDF4 as nc
+from scipy.stats import pearsonr
+import numpy as np
+import datetime as dt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import cartopy.mpl.ticker as cticker
+import matplotlib.pyplot as plt
+import xarray as xr
+from cartopy.util import add_cyclic_point 
+import matplotlib.path as mpath
+from cartopy.mpl import geoaxes
+import matplotlib.ticker as mticker
+from matplotlib import patches
+from scipy.stats.mstats import ttest_ind
+import matplotlib as mpl
+from matplotlib.colors import ListedColormap
+import cmaps
+
+f1=xr.open_dataset('D:/data/ERA5/total precipitation1951-2021.nc')
+def sp(str1,str2,str3,str4,str5,str6,str7,str8,str9,str10,str11,str12,str13,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13):
+    pre_cli=np.array(f1.tp.loc['1979-01-01':'2021-12-01',80:0,:]).reshape(43,12,321,1440).mean((0))
+    e1=np.array(f1.tp.loc[str1,80:0,:])
+    e2=np.array(f1.tp.loc[str2,80:0,:])
+    e3=np.array(f1.tp.loc[str3,80:0,:])
+    e4=np.array(f1.tp.loc[str4,80:0,:])
+    e5=np.array(f1.tp.loc[str5,80:0,:])
+    e6=np.array(f1.tp.loc[str6,80:0,:])
+    e7=np.array(f1.tp.loc[str7,80:0,:])
+    e8=np.array(f1.tp.loc[str8,80:0,:])
+    e9=np.array(f1.tp.loc[str9,80:0,:])
+    e10=np.array(f1.tp.loc[str10,80:0,:])
+    e11=np.array(f1.tp.loc[str11,80:0,:])
+    e12=np.array(f1.tp.loc[str12,80:0,:])
+    e13=np.array(f1.tp.loc[str13,80:0,:])
+    
+    e_act=(e1+e2+e3+e4+e5+e6+e7+e8+e9+e10+e11+e12+e13)/13
+    
+
+    e1_ano=(e1-pre_cli[n1,:,:]).reshape(1,321,1440)
+    e2_ano=(e2-pre_cli[n2,:,:]).reshape(1,321,1440)
+    e3_ano=(e3-pre_cli[n3,:,:]).reshape(1,321,1440)
+    e4_ano=(e4-pre_cli[n4,:,:]).reshape(1,321,1440)
+    e5_ano=(e5-pre_cli[n5,:,:]).reshape(1,321,1440)
+    e6_ano=(e6-pre_cli[n6,:,:]).reshape(1,321,1440)
+    e7_ano=(e7-pre_cli[n7,:,:]).reshape(1,321,1440)
+    e8_ano=(e8-pre_cli[n8,:,:]).reshape(1,321,1440)
+    e9_ano=(e9-pre_cli[n9,:,:]).reshape(1,321,1440)
+    e10_ano=(e10-pre_cli[n10,:,:]).reshape(1,321,1440)
+    e11_ano=(e11-pre_cli[n11,:,:]).reshape(1,321,1440)
+    e12_ano=(e12-pre_cli[n12,:,:]).reshape(1,321,1440)
+    e13_ano=(e13-pre_cli[n13,:,:]).reshape(1,321,1440)
+
+    e_ano=np.concatenate((e1_ano,e2_ano,e3_ano,e4_ano,e5_ano,e6_ano,e7_ano,e8_ano,e9_ano,e10_ano,e11_ano,e12_ano,e13_ano),axis=0)
+    e_ano_mean=e_ano.mean((0))
+    cyclic_data,cyclic_lons=add_cyclic_point(e_ano_mean,coord=lon)
+    a=np.zeros((321,1440))
+    t,p=ttest_ind(e_ano,a,equal_var=False)
+    cyclic_p,cyclic_lons=add_cyclic_point(p,coord=lon)
+    
+    return cyclic_data,cyclic_p,cyclic_lons
+
+
+
+lat=np.array(f1.latitude.loc[80:0])
+lon=np.array(f1.longitude.loc[:])
+
+fig = plt.figure(figsize=(12,12))
+proj = ccrs.PlateCarree(central_longitude=0)
+
+
+cyclic_data,cyclic_p,cyclic_lons=sp('1956-11-01','1957-11-01','1990-11-01','2013-11-01','2014-11-01','1961-11-01','1962-11-01','1985-11-01','1989-11-01','1993-11-01','2004-11-01','2019-11-01','2020-11-01',10,10,10,10,10,10,10,10,10,10,10,10,10)
+u1,v1=np.zeros((321,1440)),np.zeros((321,1440))
+font={'family':'Arial'}
+ax2 = fig.add_axes([0.3, 0.75, 0.6, 0.6],projection = proj)
+ax2.add_feature(cfeature.COASTLINE.with_scale('50m'),lw=0.5,color='k')#线宽lw
+ax2.add_feature(cfeature.LAND,facecolor='gray')#给陆地填色
+ax2.set_extent([-30,150,0,80],crs=ccrs.PlateCarree())
+ax2.set_xticks(np.array([-30,0,30,60,90,120,150]),crs=ccrs.PlateCarree())
+ax2.set_yticks(np.array([0,20,40,60,80]),crs=ccrs.PlateCarree())
+ax2.set_xticklabels([-30,0,30,60,90,120,150],fontdict=font)
+ax2.set_yticklabels([0,20,40,60,80],fontdict=font)
+ax2.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+ax2.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+c2=ax2.contourf(cyclic_lons,lat,cyclic_data*30*1000,transform=ccrs.PlateCarree(),extend='both',levels=np.arange(-30,36,6),zorder=0,cmap='BrBG')
+d=patches.Rectangle((0,35),30,10,linewidth=2,linestyle='-',zorder=4,edgecolor='lime',facecolor='none',transform=ccrs.PlateCarree())
+ax2.add_patch(d)
+
+d=patches.Rectangle((334,48),26,17,linewidth=2,linestyle='-',zorder=4,edgecolor='k',facecolor='none',transform=ccrs.PlateCarree())
+ax2.add_patch(d)
+
+ax2.tick_params(axis='x',labelsize=14,width=1.5,length=7)
+ax2.tick_params(axis='y',labelsize=14,width=1.5,length=7)
+
+
+ax2.spines['bottom'].set_visible(True)
+ax2.spines['left'].set_visible(True)
+ax2.spines['right'].set_visible(True)
+ax2.spines['top'].set_visible(True)
+ax2.spines['left'].set_linewidth(1.5)
+ax2.spines['right'].set_linewidth(1.5)
+ax2.spines['top'].set_linewidth(1.5)
+ax2.spines['bottom'].set_linewidth(1.5)
+#ax2.set_title('(a)',loc='left',fontsize=16,fontproperties='Arial')
+#ax2.set_title('(a) Nov',loc='left',fontsize=16,fontproperties='Arial')
+#ax2.set_title('Precipitation',loc='right',fontsize=16,fontproperties='Arial')
+for i in range(0,321,10):
+    for j in range(0,1440,10):
+        if j<=720 and cyclic_p[i,j]<=0.1:
+            ax2.plot(lon[j],lat[i],marker='o',markersize=1,c='r')
+        if j>720 and cyclic_p[i,j]<=0.1:
+            ax2.plot(lon[j]-360,lat[i],marker='o',markersize=1,c='r')
+
+for i in range(0,321,5):
+    for j in range(0,1440,5):
+        if 140<=i<=180 and 0<=j<=120 and cyclic_p[i,j]<=0.1:
+            ax2.plot(lon[j],lat[i],marker='o',markersize=1,c='r')
+position=fig.add_axes([0.91,0.915,0.015,0.27])
+clb=plt.colorbar(c2,cax=position,orientation='vertical',format='%.0f',ticks=[-30,-24,-18,-12,-6,0,6,12,18,24,30])
+clb.ax.tick_params(labelsize=14,width=1.5)  
+
+
+
+plt.savefig('D:/figure/改文章6/11月降水.pdf',dpi=300,bbox_inches='tight')  
---
+++ fig 5.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 09:16:55 2023
+
+@author: HH
+"""
+
+from scipy.stats import pearsonr
+import numpy as np
+import datetime as dt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import cartopy.mpl.ticker as cticker
+import matplotlib.pyplot as plt
+import xarray as xr
+from cartopy.util import add_cyclic_point 
+import matplotlib.path as mpath
+from cartopy.mpl import geoaxes
+import matplotlib.ticker as mticker
+from matplotlib import patches
+from scipy import signal
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from scipy.stats import zscore
+import pandas as pd
+from scipy.stats.mstats import ttest_ind
+import matplotlib as mpl
+from matplotlib.colors import ListedColormap
+import cmaps
+from scipy.stats import linregress
+
+
+f1=xr.open_dataset('D:/data/FAMIPC5_clim_VerticalTransfer_Z3_monthly.nc')
+f2=xr.open_dataset('D:/data/FAMIPC5_NA_composite_REALwarm_VerticalTransfer_Z3_monthly.nc')
+f3=xr.open_dataset('D:/FAMIPC5_Med_composite_REALwarm_VerticalTransfer_Z3_monthly.nc')
+f4=xr.open_dataset('D:/data/FAMIPC5_inputdata_forcing_climatol_1x1_2.nc')
+f5=xr.open_dataset('D:/FAMIPC5_inputdata_REALforcing_NA_composite_1x1.nc')
+f6=xr.open_dataset('D:/FAMIPC5_inputdata_REALforcing_Med_composite_1x1.nc')
+
+
+lat=np.array(f2.lat.loc[-1:90])
+lon=np.array(f2.lon.loc[:])
+
+lat1=np.array(f4.lat.loc[0:90])
+lon1=np.array(f4.lon.loc[:])
+
+z_c=np.array(f1.Z3.loc[10:11,300,-1:90,:])
+z_na=np.array(f2.Z3.loc[10:11,300,-1:90,:])
+z_na_ano=z_na-z_c
+z_na_ano,cyclic_lons=add_cyclic_point(z_na_ano,coord=lon)
+
+
+z_med=np.array(f3.Z3.loc[10:11,300,-1:90,:])
+z_med_ano=z_med-z_c
+z_med_ano,cyclic_lons=add_cyclic_point(z_med_ano,coord=lon)
+
+sst_c=np.array(f4.SST_cpl.loc['1990-11-16',0:90,:])
+sst_na=np.array(f5.SST_cpl.loc['1990-11-16',0:90,:])
+sst_na_ano=(sst_na-sst_c).reshape(90,360)
+a=np.isnan(sst_na_ano)
+for i in range(0,90):
+    for j in range(0,360):
+        if a[i,j]==True:
+            sst_na_ano[i,j]=0
+sst_na_ano,cyclic_lons1=add_cyclic_point(sst_na_ano,coord=lon1)
+
+
+sst_med=np.array(f6.SST_cpl.loc['1990-11-16',0:90,:])
+sst_med_ano=(sst_med-sst_c).reshape(90,360)
+a=np.isnan(sst_med_ano)
+for i in range(0,90):
+    for j in range(0,360):
+        if a[i,j]==True:
+            sst_med_ano[i,j]=0
+sst_med_ano,cyclic_lons1=add_cyclic_point(sst_med_ano,coord=lon1)
+
+cmap=cmaps.BlueWhiteOrangeRed 
+cmap1=cmaps.BlueDarkRed18
+
+fig = plt.figure(figsize=(12,8))
+proj = ccrs.PlateCarree(central_longitude=15)
+proj2 = ccrs.PlateCarree(central_longitude=-15)
+proj1 = ccrs.PlateCarree(central_longitude=120)
+
+sst_med_ano[sst_med_ano==0]=np.nan
+sst_na_ano[sst_na_ano==0]=np.nan
+font={'family':'Arial'}
+ax = fig.add_axes([0.1, 0.6, 0.4, 0.4],projection = proj)
+ax.add_feature(cfeature.COASTLINE.with_scale('50m'),lw=1)#线宽lw
+ax.add_feature(cfeature.LAND,color='w')#给陆地填色
+ax.set_extent([-30,60,20,60],crs=ccrs.PlateCarree())
+ax.set_xticks(np.arange(-30,90,30),crs=ccrs.PlateCarree())
+ax.set_yticks(np.array([20,40,60]),crs=ccrs.PlateCarree())
+ax.set_xticklabels(np.arange(-30,90,30),fontdict=font)
+ax.set_yticklabels([20,40,60],fontdict=font)
+ax.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+ax.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+c1=ax.contourf(cyclic_lons1,lat1,sst_med_ano,transform=ccrs.PlateCarree(),cmap=cmap,zorder=0,levels=np.arange(-1,1.1,0.1))
+ax.tick_params(labelsize=14,width=1.5,length=6)
+ax.set_title('(a) Exp_Med',loc='left',fontsize=16,fontproperties='Arial')
+ax.set_title('Nov',loc='center',fontsize=16,fontproperties='Arial')
+ax.set_title('SST',loc='right',fontsize=16,fontproperties='Arial')
+
+
+
+ax1 = fig.add_axes([0.56, 0.61, 0.38, 0.38],projection = proj2)
+ax1.add_feature(cfeature.COASTLINE.with_scale('50m'),lw=1)#线宽lw
+ax1.add_feature(cfeature.LAND,color='w')#给陆地填色
+ax1.set_extent([-120,30,10,80],crs=ccrs.PlateCarree())
+ax1.set_xticks(np.arange(-120,60,30),crs=ccrs.PlateCarree())
+ax1.set_yticks(np.array([20,40,60,80]),crs=ccrs.PlateCarree())
+ax1.set_xticklabels(np.arange(-120,60,30),fontdict=font)
+ax1.set_yticklabels([20,40,60,80],fontdict=font)
+ax1.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+ax1.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+#c1=ax1.contourf(cyclic_lons1,lat1,sst_na_ano,transform=ccrs.PlateCarree(),cmap='RdBu_r',zorder=0,levels=np.arange(-0.7,0.71,0.1),extend='both')
+c1=ax1.contourf(cyclic_lons1,lat1,sst_na_ano,transform=ccrs.PlateCarree(),levels=np.arange(-1,1.1,0.1),zorder=0,cmap=cmap,extend='both')
+
+ax1.tick_params(labelsize=14,width=1.5,length=6)
+ax1.set_title('(d) Exp_NAtl',loc='left',fontsize=16,fontproperties='Arial')
+ax1.set_title('Nov',loc='center',fontsize=16,fontproperties='Arial')
+ax1.set_title('SST',loc='right',fontsize=16,fontproperties='Arial')
+
+
+position=fig.add_axes([0.965,0.68,0.01,0.25])
+clb=plt.colorbar(c1,cax=position,orientation='vertical',format='%.1f',ticks=[-1.2,-0.8,-0.4,0,0.4,0.8,1.2])
+clb.ax.tick_params(labelsize=14,width=1.5)
+
+
+ax2 = fig.add_axes([0.095, 0.3, 0.4, 0.4],projection = proj1)
+ax2.add_feature(cfeature.COASTLINE.with_scale('50m'),lw=1)#线宽lw
+ax2.add_feature(cfeature.LAND,color='gray')#给陆地填色
+#ax2.set_extent([-60,60,0,80],crs=ccrs.PlateCarree())
+ax2.set_xticks(np.arange(-60,360,60),crs=ccrs.PlateCarree())
+ax2.set_yticks(np.array([0,30,60,90]),crs=ccrs.PlateCarree())
+ax2.set_xticklabels(np.arange(-60,360,60),fontdict=font)
+ax2.set_yticklabels([0,30,60,90],fontdict=font)
+ax2.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+ax2.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+c1=ax2.contourf(cyclic_lons,lat,z_med_ano[0],transform=ccrs.PlateCarree(),cmap=cmap,zorder=0,levels=np.arange(-60,70,10),extend='both')
+ax2.tick_params(labelsize=14,width=1.5,length=6)
+ax2.set_title('(b) Exp_Med',loc='left',fontsize=16,fontproperties='Arial')
+ax2.set_title('Oct',loc='center',fontsize=16,fontproperties='Arial')
+ax2.set_title('500hPa HGT',loc='right',fontsize=16,fontproperties='Arial')
+
+
+ax3 = fig.add_axes([0.555, 0.3, 0.4, 0.4],projection = proj1)
+ax3.add_feature(cfeature.COASTLINE.with_scale('50m'),lw=1)#线宽lw
+ax3.add_feature(cfeature.LAND,color='gray')#给陆地填色
+#ax3.set_extent([-60,60,0,80],crs=ccrs.PlateCarree())
+ax3.set_xticks(np.arange(-60,360,60),crs=ccrs.PlateCarree())
+ax3.set_yticks(np.array([0,30,60,90]),crs=ccrs.PlateCarree())
+ax3.set_xticklabels(np.arange(-60,360,60),fontdict=font)
+ax3.set_yticklabels([0,30,60,90],fontdict=font)
+ax3.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+ax3.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+c1=ax3.contourf(cyclic_lons,lat,z_na_ano[0],transform=ccrs.PlateCarree(),cmap=cmap,zorder=0,levels=np.arange(-60,70,10),extend='both')
+ax3.tick_params(labelsize=14,width=1.5,length=6)
+ax3.set_title('(e) Exp_NAtl',loc='left',fontsize=16,fontproperties='Arial')
+ax3.set_title('Oct',loc='center',fontsize=16,fontproperties='Arial')
+ax3.set_title('500hPa HGT',loc='right',fontsize=16,fontproperties='Arial')
+
+
+
+
+ax4 = fig.add_axes([0.095, 0.055, 0.4, 0.4],projection = proj1)
+ax4.add_feature(cfeature.COASTLINE.with_scale('50m'),lw=1)#线宽lw
+ax4.add_feature(cfeature.LAND,color='gray')#给陆地填色
+#ax4.set_extent([-60,60,0,80],crs=ccrs.PlateCarree())
+ax4.set_xticks(np.arange(-60,360,60),crs=ccrs.PlateCarree())
+ax4.set_yticks(np.array([0,30,60,90]),crs=ccrs.PlateCarree())
+ax4.set_xticklabels(np.arange(-60,360,60),fontdict=font)
+ax4.set_yticklabels([0,30,60,90],fontdict=font)
+ax4.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+ax4.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+c1=ax4.contourf(cyclic_lons,lat,z_med_ano[1],transform=ccrs.PlateCarree(),cmap=cmap,zorder=0,levels=np.arange(-60,70,10),extend='both')
+ax4.tick_params(labelsize=14,width=1.5,length=6)
+ax4.set_title('(c) Exp_Med',loc='left',fontsize=16,fontproperties='Arial')
+ax4.set_title('Nov',loc='center',fontsize=16,fontproperties='Arial')
+ax4.set_title('500hPa HGT',loc='right',fontsize=16,fontproperties='Arial')
+
+
+ax5 = fig.add_axes([0.555, 0.055, 0.4, 0.4],projection = proj1)
+ax5.add_feature(cfeature.COASTLINE.with_scale('50m'),lw=1)#线宽lw
+ax5.add_feature(cfeature.LAND,color='gray')#给陆地填色
+#ax5.set_extent([-60,60,0,80],crs=ccrs.PlateCarree())
+ax5.set_xticks(np.arange(-60,360,60),crs=ccrs.PlateCarree())
+ax5.set_yticks(np.array([0,30,60,90]),crs=ccrs.PlateCarree())
+ax5.set_xticklabels(np.arange(-60,360,60),fontdict=font)
+ax5.set_yticklabels([0,30,60,90],fontdict=font)
+ax5.xaxis.set_major_formatter(cticker.LongitudeFormatter())
+ax5.yaxis.set_major_formatter(cticker.LatitudeFormatter())
+c1=ax5.contourf(cyclic_lons,lat,z_na_ano[1],transform=ccrs.PlateCarree(),cmap=cmap,zorder=0,levels=np.arange(-60,70,10),extend='both')
+ax5.tick_params(labelsize=14,width=1.5,length=6)
+ax5.set_title('(f) Exp_NAtl',loc='left',fontsize=16,fontproperties='Arial')
+ax5.set_title('Nov',loc='center',fontsize=16,fontproperties='Arial')
+ax5.set_title('500hPa HGT',loc='right',fontsize=16,fontproperties='Arial')
+
+position=fig.add_axes([0.965,0.23,0.01,0.3])
+clb=plt.colorbar(c1,cax=position,orientation='vertical',format='%.0f')
+clb.ax.tick_params(labelsize=14,width=1.5)
+
+
+
+#plt.savefig('D:/figure/改文章6/新的FAMIPC5sst和hgt组图.png',bbox_inches='tight',dpi=300)
+
+
+
+
+
+
+
+
+
+
+
+
+
